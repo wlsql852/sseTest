1e9708d257f0d719bb1ff33aa0fb293d0cb4fcbd
wlsql852@naver.com <wlsql852@naver.com>
2024-10-27 23:22:26 +0900

    초기 코드

---
+++ src/test/java/com/sparta/ssetest/SseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ssetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
