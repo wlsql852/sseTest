4b5abefa71922ab74dc4aabe322f4a93342e28ff
wlsql852@naver.com <wlsql852@naver.com>
2024-10-28 10:33:57 +0900

    출처 1 원본 코드 작성(PostLike가 없어서 실행은 못함)

--- src/main/java/com/sparta/ssetest/entity/User.java
+++ src/main/java/com/sparta/ssetest/entity/User.java
@@ -1,4 +1,4 @@
-package com.sparta.ssetest;
+package com.sparta.ssetest.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
--- src/main/java/com/sparta/ssetest/repository/UserRepository.java
+++ src/main/java/com/sparta/ssetest/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.sparta.ssetest;
+package com.sparta.ssetest.repository;
 
+import com.sparta.ssetest.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
